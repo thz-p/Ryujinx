a0ca005c68dab5ba6dc17f54b1ad1a80037f5c3f
thz-p <872846303@QQ.COM>
2024-03-03 03:42:00 +0000

    t

--- src/Ryujinx.UI.Common/UserError.cs
+++ src/Ryujinx.UI.Common/UserError.cs
@@ -1,38 +1,38 @@
 namespace Ryujinx.UI.Common
 {
     /// <summary>
-    /// Represent a common error that could be reported to the user by the emulator.
+    /// 表示模拟器可能向用户报告的常见错误。
     /// </summary>
     public enum UserError
     {
         /// <summary>
-        /// No error to report.
+        /// 没有错误需要报告。
         /// </summary>
         Success = 0x0,
 
         /// <summary>
-        /// No keys are present.
+        /// 没有密钥。
         /// </summary>
         NoKeys = 0x1,
 
         /// <summary>
-        /// No firmware is installed.
+        /// 没有固件。
         /// </summary>
         NoFirmware = 0x2,
 
         /// <summary>
-        /// Firmware parsing failed.
+        /// 固件解析失败。
         /// </summary>
-        /// <remarks>Most likely related to keys.</remarks>
+        /// <remarks>可能与密钥相关。</remarks>
         FirmwareParsingFailed = 0x3,
 
         /// <summary>
-        /// No application was found at the given path.
+        /// 给定路径中找不到应用程序。
         /// </summary>
         ApplicationNotFound = 0x4,
 
         /// <summary>
-        /// An unknown error.
+        /// 未知错误。
         /// </summary>
         Unknown = 0xDEAD,
     }
